9abb1ffeccf46c1df2eb3f013c1e5636f4f91934
Dima <76776847+DimaK0489@users.noreply.github.com>
2026-05-12 21:12:02 +0300

    completed Router page

--- src/constants/codeExample.ts
+++ src/constants/codeExample.ts
@@ -158,3 +158,75 @@ function App() {
     </BrowserRouter>
   );
 }`;
+
+export const useParamsCode = `import { useParams } from 'react-router-dom';  
+
+function User() {  
+  const { userId } = useParams();  
+
+  return <h2>User ID: {userId}</h2>;  
+}`;
+
+export const useNavigateCode = `import { useNavigate } from 'react-router-dom';  
+
+function Login() {  
+  const navigate = useNavigate();  
+
+  const handleLogin = () => {  
+    // Логика входа...  
+    navigate('/dashboard'); // Перейти на страницу кабинета после входа  
+  };  
+
+  return <button onClick={handleLogin}>Login</button>;  
+}`;
+
+export const useLocationCode = `import { useLocation } from 'react-router-dom';  
+
+function CurrentLocation() {  
+  const location = useLocation();  
+
+  return <div>Current location: {location.pathname}</div>;  
+}`;
+
+export const useRoutes = `import { useRoutes } from 'react-router-dom';  
+
+function AppRoutes() {  
+  const routes = [  
+    { path: '/', element: <Home /> },  
+    { path: '/about', element: <About /> },  
+    { path: '/contact', element: <Contact /> },  
+  ];  
+
+  return useRoutes(routes);  
+}`;
+
+export const useSearchParamsCode = `import { useSearchParams } from 'react-router-dom';  
+
+function SearchPage() {  
+  // Хук для работы с параметрами поискового запроса  
+  const [searchParams, setSearchParams] = useSearchParams();  
+
+  // Извлекаем параметр 'query' из поисковых параметров  
+  const query = searchParams.get('query') || '';  
+
+  const handleSearch = (event) => {  
+    event.preventDefault();  
+    // Изменяем параметр 'query' в URL  
+    setSearchParams({ query: event.target.searchInput.value });  
+  };  
+
+  return (  
+    <div>  
+      <h1>Search Page</h1>  
+      <form onSubmit={handleSearch}>  
+        <input  
+          type="text"  
+          name="searchInput"  
+          defaultValue={query} // Значение по умолчанию для инпута  
+        />  
+        <button type="submit">Search</button>  
+      </form>  
+      <p>Current query: {query}</p>  
+    </div>  
+  );  
+}`;
--- src/pages/RouterPage/RouterPage.tsx
+++ src/pages/RouterPage/RouterPage.tsx
@@ -1,5 +1,13 @@
+import { DocLink } from "../../components/DocLink/DocLink";
 import { TopicHeader } from "../../components/TopicHeader/TopicHeader";
-import { routerCode } from "../../constants/codeExample";
+import {
+  routerCode,
+  useLocationCode,
+  useNavigateCode,
+  useParamsCode,
+  useRoutes,
+  useSearchParamsCode,
+} from "../../constants/codeExample";
 import s from "./RouterPage.module.css";
 
 export const RouterPage = () => {
@@ -24,6 +32,10 @@ export const RouterPage = () => {
             <span className={s.compTag}>Link & NavLink</span>
             <p>Замена тегу &lt;a&gt;. Меняет URL без перезагрузки страницы.</p>
           </div>
+          <div className={s.compCard}>
+            <span className={s.compTag}>Outlet</span>
+            <p>Используется для рендеринга вложенных маршрутов.</p>
+          </div>
         </div>
       </section>
       <section className={s.section}>
@@ -32,6 +44,66 @@ export const RouterPage = () => {
           <code>{routerCode}</code>
         </pre>
       </section>
+      <section className={s.section}>
+        <h2 className={s.subtitle}>Основные хуки</h2>
+        <div className={s.hooksWrapper}>
+          <div className={s.hookItem}>
+            <span className={s.hookName}>useParams:</span>
+            <p className={s.hookDescription}>
+              Используется для извлечения параметров URL в компонентах
+              маршрутов.
+            </p>
+            <pre className={s.code}>
+              <code>{useParamsCode}</code>
+            </pre>
+          </div>
+          <div className={s.hookItem}>
+            <span className={s.hookName}>useNavigate:</span>
+            <p className={s.hookDescription}>
+              Позволяет программно изменять маршрут (например, после отправки
+              формы).
+            </p>
+            <pre className={s.code}>
+              <code>{useNavigateCode}</code>
+            </pre>
+          </div>
+          <div className={s.hookItem}>
+            <span className={s.hookName}>useLocation:</span>
+            <p className={s.hookDescription}>
+              Предоставляет информацию о текущем URL, такой как pathname и
+              состояние.
+            </p>
+            <pre className={s.code}>
+              <code>{useLocationCode}</code>
+            </pre>
+          </div>
+          <div className={s.hookItem}>
+            <span className={s.hookName}>useRoutes:</span>
+            <p className={s.hookDescription}>
+              Позволяет определять маршруты в виде массива, что может быть
+              удобно для управления маршрутами в более сложных приложениях.
+            </p>
+            <pre className={s.code}>
+              <code>{useRoutes}</code>
+            </pre>
+          </div>
+          <div className={s.hookItem}>
+            <span className={s.hookName}>useSearchParams:</span>
+            <p className={s.hookDescription}>
+              Чтобы использовать useSearchParams, вам нужно импортировать его из
+              react-router-dom. Вот пример того, как вы можете использовать его
+              для извлечения и изменения параметров поискового запроса.
+            </p>
+            <pre className={s.code}>
+              <code>{useSearchParamsCode}</code>
+            </pre>
+          </div>
+        </div>
+      </section>
+      <DocLink
+        href="https://reactrouter.com/home"
+        title="Документация React-router"
+      />
     </article>
   );
 };
